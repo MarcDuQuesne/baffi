ec74ae73b0ae51fb81d2feb3eb566c8d285870a2
Matteo Giani <matteo.giani.87@gmail.com>
2022-08-26 11:06:14 +0200

    Added a couple of decorators

--- baffi/decorators/log_helpers.py
+++ baffi/decorators/log_helpers.py
@@ -20,8 +20,53 @@ def log_wrapper(func,
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        pre_format and logger.log(msg=pre_format.format(**locals(), **wrapper_kwargs), level=level)
+        pre_format and logger.log(msg=pre_format.format(**locals(), **wrapper_kwargs, **kwargs), level=level)
         result = func(*args, **kwargs)
-        post_format and logger.log(msg=post_format.format(**locals(), **wrapper_kwargs), level=level)
+        post_format and logger.log(msg=post_format.format(**locals(), **wrapper_kwargs, **kwargs), level=level)
         return result
+    return wrapper
+
+  
+@parametrized
+def timeit(
+    func,
+    format="{func.__name__} executed in {duration:.4f}s",
+    level=logging.INFO,
+    **wrapper_kwargs,
+):
+    """
+    Logs the execution time of a function.
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        duration = t2 - t1  # (t2-t1) won't work with format
+        logger.log(
+            msg=format.format(**locals(), **wrapper_kwargs, **kwargs), level=level
+        )
+        return result
+
+    return wrapper
+  
+
+@parametrized
+def exc_pre_post(func,
+                 pre=None,
+                 post=None,
+                 **wrapper_kwargs
+):
+    """
+    Executes arbitrary functions before and after a given one.
+    """
+    
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        _pre = pre(*args, **kwargs, **wrapper_kwargs) if pre else None
+        result = func(*args, **kwargs)
+        post and post(*args, **kwargs, **wrapper_kwargs, pre=_pre)
+        return result
+
     return wrapper
