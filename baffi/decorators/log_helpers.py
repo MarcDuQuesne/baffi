2775101af24d30836ddc203a2d67ab98b29a855d
Matteo Giani <matteo.giani@dsm.com>
2022-09-17 23:20:12 +0200

    Experience dictat.

--- baffi/decorators/log_helpers.py
+++ baffi/decorators/log_helpers.py
@@ -1,18 +1,57 @@
-
 from functools import wraps
 import logging
+from inspect import getframeinfo, stack
+import os
+import sys
+from time import time
+from functools import partial
 
 from baffi.decorators.core import parametrized
 
+
+class OverridingFormatter(logging.Formatter):
+    """
+    1. Overrides 'funcName' with the value of 'func_name_override', if it exists.
+    2. Overrides 'filename' with the value of 'file_name_override', if it exists.
+    """
+
+    def format(self, record):
+        if hasattr(record, "name_override"):
+            record.name = record.name_override
+        if hasattr(record, "func_name_override"):
+            record.funcName = record.func_name_override
+        if hasattr(record, "file_name_override"):
+            record.filename = record.file_name_override
+        if hasattr(record, "module_override"):
+            record.module = record.module_override
+        if hasattr(record, "pathname_override"):
+            record.pathname = record.pathname_override
+
+        return super(OverridingFormatter, self).format(record)
+
+
 logger = logging.getLogger(__name__)
+logger.propagate = False  # Avoids duplicate messages
+handler = logging.StreamHandler(sys.stderr)
+format = (
+    logging.BASIC_FORMAT
+    if logging.root.handlers is None
+    else logging.root.handlers[0].formatter._fmt
+)  # Taking the first one.
+handler.setFormatter(OverridingFormatter(format))
+logger.addHandler(handler)
 
 
 @parametrized
-def log_wrapper(func,
-                pre_format='',
-                post_format='{func.__name__}: {result}',
-                level=logging.INFO,
-                **wrapper_kwargs):
+def log_wrapper(
+    func,
+    pre_format="",
+    post_format="{func.__name__}: {result}",
+    # logger=None,
+    format=None,
+    level=logging.INFO,
+    **wrapper_kwargs
+):
     """
     Decorator that adds a formatted log line before/after running a function
     for debugging purposes.
@@ -20,48 +59,45 @@ def log_wrapper(func,
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        pre_format and logger.log(msg=pre_format.format(**locals(), **wrapper_kwargs, **kwargs), level=level)
-        result = func(*args, **kwargs)
-        post_format and logger.log(msg=post_format.format(**locals(), **wrapper_kwargs, **kwargs), level=level)
-        return result
-    return wrapper
 
-  
-@parametrized
-def timeit(
-    func,
-    format="{func.__name__} executed in {duration:.4f}s",
-    level=logging.INFO,
-    **wrapper_kwargs,
-):
-    """
-    Logs the execution time of a function.
-    """
+        py_file_caller = getframeinfo(stack()[1][0])
+        extra_args = {
+            "name_override": os.path.basename(func.__module__),
+            "func_name_override": func.__name__,
+            "file_name_override": os.path.basename(py_file_caller.filename),
+            "module_name_override": os.path.basename(func.__module__),
+            # 'pathname_override':
+        }
 
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        t1 = time()
-        result = func(*args, **kwargs)
-        t2 = time()
-        duration = t2 - t1  # (t2-t1) won't work with format
-        logger.log(
-            msg=format.format(**locals(), **wrapper_kwargs, **kwargs), level=level
+        start = time()
+        pre_format and logger.log(
+            msg=pre_format.format(**locals(), **wrapper_kwargs, **kwargs),
+            level=level,
+            extra=extra_args,
         )
+        result = func(*args, **kwargs)
+        end = time()
+        duration = end - start
+        post_format and logger.log(
+            msg=post_format.format(**locals(), **wrapper_kwargs, **kwargs),
+            level=level,
+            extra=extra_args,
+        )
+
         return result
 
     return wrapper
-  
+
+
+timeit = log_wrapper(post_format="Duration {duration} s.")
+
 
 @parametrized
-def exc_pre_post(func,
-                 pre=None,
-                 post=None,
-                 **wrapper_kwargs
-):
+def exc_pre_post(func, pre=None, post=None, **wrapper_kwargs):
     """
     Executes arbitrary functions before and after a given one.
     """
-    
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         _pre = pre(*args, **kwargs, **wrapper_kwargs) if pre else None
--- baffi/metaclasses/autodecorator.py
+++ baffi/metaclasses/autodecorator.py
@@ -1,9 +1,19 @@
-def decorating_meta(decorator):
+"""
+Interesting metaclasses.
+"""
+
+
+def apply_to_each_function(*decorators):
     class DecoratingMetaclass(type):
         def __new__(self, class_name, bases, namespace):
             for key, value in list(namespace.items()):
-                if callable(value):
-                    namespace[key] = decorator(value)
+                for decorator in decorators:
+                    if callable(value):
+                        namespace[key] = decorator(value)
+                    elif isinstance(value, classmethod):
+                        namespace[key] = classmethod(decorator(value.__func__))
+                    elif isinstance(value, staticmethod):
+                        namespace[key] = staticmethod(decorator(value.__func__))
 
             return type.__new__(self, class_name, bases, namespace)
 
@@ -12,17 +22,16 @@ def decorating_meta(decorator):
 
 if __name__ == "__main__":
 
-  import logging
-  logging.basicConfig(level=logging.DEBUG)
+    import logging
 
-  from baffi.decorators.log_helpers import log_wrapper
+    logging.basicConfig(level=logging.DEBUG)
 
-  class Foo(dict, metaclass=decorating_meta(log_wrapper(level=logging.INFO))):
+    from baffi.decorators.log_helpers import log_wrapper
 
-    def lookup(self, key):
-        return self[key]
+    class Foo(dict, metaclass=apply_to_each_function(log_wrapper(level=logging.INFO))):
+        def lookup(self, key):
+            return self[key]
 
-  d = Foo()
-  d['3'] = 2
-  d.lookup('3')
-
+    d = Foo()
+    d["3"] = 2
+    d.lookup("3")
