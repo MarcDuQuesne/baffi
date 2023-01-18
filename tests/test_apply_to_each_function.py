c6d98258ba1ec9c589672c4af95f5e68366e1f32
Matteo Giani <matteo.giani@dsm.com>
2023-01-18 11:08:54 +0100

    Test for the apply_to_each_function decorator.

--- tests/test_apply_to_each_function.py
+++ tests/test_apply_to_each_function.py
@@ -2,17 +2,35 @@
 Test the apply_to_each_function decorator
 """
 
+from functools import wraps
 
 import pytest
 
+from baffi.metaclasses.autodecorator import apply_to_each_function
+
+
+@pytest.mark.integration
 def test_apply_to_each_function():
+    """
+    Test the apply_to_each_function decorator
+    """
+    def return_2(func):
+        """
+        Dummy decorator for the test
+        """
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return 2
+        return wrapper
 
-    def 
+    class Foo(dict, metaclass=apply_to_each_function(return_2)):
+        """
+        Dummy class for the test
+        """
+        def return_3(self):
+            """
+            Dummy function for the test
+            """
+            return 3
 
-    class Foo(dict, metaclass=apply_to_each_function(log_wrapper(level=logging.INFO))):
-        def lookup(self, key):
-            return self[key]
-
-    d = Foo()
-    d["3"] = 2
-    d.lookup("3")
+    assert Foo().return_3() == 2, "The function was not applied to the class."
